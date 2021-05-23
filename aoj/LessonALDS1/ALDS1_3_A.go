17cd678947361a22991503b31514e8624a4cec70
norun111 <u.t012807@gmail.com>
2021-05-23 14:31:26 +0900

    orerand

---
+++ aoj/LessonALDS1/ALDS1_3_A.go
@@ -0,0 +1,35 @@
+package LessonALDS1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func operand() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	list := strings.Split(sc.Text(), " ")
+	op := make([]int, 100)
+	p := 0
+	n := len(list)
+	for i := 0; i < n; i++ {
+		switch list[i] {
+		case "+":
+			op[p-2] += op[p-1]
+			p--
+		case "-":
+			op[p-2] -= op[p-1]
+			p--
+		case "*":
+			op[p-2] *= op[p-1]
+			p--
+		default:
+			op[p], _ = strconv.Atoi(list[i])
+			p++
+		}
+	}
+	fmt.Println(op[0])
+}
