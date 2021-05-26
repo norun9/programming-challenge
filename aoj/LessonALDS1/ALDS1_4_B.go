1bcae4a789e4b9eff7002418774001657b8762ff
norun111 <u.t012807@gmail.com>
2021-05-26 14:52:24 +0900

    binary search2

--- aoj/LessonALDS1/ALDS1_4_B.go
+++ aoj/LessonALDS1/ALDS1_4_B.go
@@ -8,41 +8,68 @@ import (
 	"strings"
 )
 
-func binSearch(tcd int, A []string) int {
-	low := 0
-	high := len(A) - 1
-	for low <= high {
-		mid := (low + high) / 2
-		a, _ := strconv.Atoi(A[mid])
-		guess := a
-		if guess == tcd {
-			return mid
-		}
-		if guess > tcd {
-			high = mid - 1
-		} else {
-			low = mid + 1
+func getNextLine(scanner *bufio.Reader) string {
+	var buffer string
+	for {
+		line, isPrefix, _ := scanner.ReadLine()
+		buffer += string(line)
+		if isPrefix == false {
+			break
 		}
 	}
-	return -1
+	return buffer
 }
 
-func bin() {
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Scan()
-	sc.Scan()
-	S := strings.Split(sc.Text(), " ")
+func getIntList(scanner *bufio.Reader) []int {
+	list := strings.Split(getNextLine(scanner), " ")
+	l := len(list)
+	result := make([]int, l)
+	for i := 0; i < l; i++ {
+		result[i], _ = strconv.Atoi(list[i])
+	}
+	return result
+}
 
-	sc.Scan()
-	sc.Scan()
-	T := strings.Split(sc.Text(), " ")
+func binSearch(A []int, tcd int) bool {
+	var first, last int
+	last = len(A) - 1
+
+	for first <= last {
+		mid := (first + last) / 2
+		if A[mid] == tcd {
+			return true
+		}
+		if A[mid] < tcd {
+			first = mid + 1
+		} else {
+			last = mid - 1
+		}
+	}
+	return false
+}
+
+func mainA() {
+	// Your code here!
+	fp := os.Stdin
+	if len(os.Args) > 1 {
+		fp, _ = os.Open(os.Args[1])
+	}
+	scanner := bufio.NewReader(fp)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var n, d int
+	var S, T []int
+	fmt.Sscan(getNextLine(scanner), &n)
+	S = getIntList(scanner)
+	fmt.Sscan(getNextLine(scanner), &d)
+	T = getIntList(scanner)
+
 	var count int
 	for _, t := range T {
-		tt, _ := strconv.Atoi(t)
-		var i int
-		if i = binSearch(tt, S); i >= 0 {
+		if binSearch(S, t) {
 			count++
 		}
 	}
 	fmt.Println(count)
+	writer.Flush()
 }
