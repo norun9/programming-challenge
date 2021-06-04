1bf70806f52c496d801bbf01b367b6040f585fe9
norun111 <u.t012807@gmail.com>
2021-06-04 22:23:56 +0900

    042

---
+++ at_corder/042/和風いろはちゃんイージー.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getNextLine(scanner *bufio.Reader) string {
+	var buffer string
+	for {
+		line, isPrefix, _ := scanner.ReadLine()
+		buffer += string(line)
+		if isPrefix == false {
+			break
+		}
+	}
+	return buffer
+}
+
+func getIntList(scanner *bufio.Reader) []int {
+	list := strings.Split(getNextLine(scanner), " ")
+	l := len(list)
+	result := make([]int, l)
+	for i := 0; i < l; i++ {
+		result[i], _ = strconv.Atoi(list[i])
+	}
+	return result
+}
+
+func find(A []int, tcd int) int {
+	I := -1
+	for i, a := range A {
+		if a == tcd {
+			I = i
+		}
+	}
+	return I
+}
+
+func irohatyan() {
+	fp := os.Stdin
+	if len(os.Args) > 1 {
+		fp, _ = os.Open(os.Args[1])
+	}
+	scanner := bufio.NewReader(fp)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var T []int
+	T = getIntList(scanner)
+	list := []int{5, 7, 5}
+	l := len(list)
+	count := 0
+	for _, t := range T {
+		result := find(list, t)
+		if result == -1 {
+			break
+		} else {
+			list = append(list[:result], list[result+1:]...)
+			count++
+		}
+	}
+	if count == l {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+	writer.Flush()
+}
---
+++ at_corder/042/文字列大好きいろはちゃんイージー.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func getNextLine(scanner *bufio.Reader) string {
+	var buffer string
+	for {
+		line, isPrefix, _ := scanner.ReadLine()
+		buffer += string(line)
+		if isPrefix == false {
+			break
+		}
+	}
+	return buffer
+}
+
+func getIntList(scanner *bufio.Reader) []int {
+	list := strings.Split(getNextLine(scanner), " ")
+	l := len(list)
+	result := make([]int, l)
+	for i := 0; i < l; i++ {
+		result[i], _ = strconv.Atoi(list[i])
+	}
+	return result
+}
+
+func getStrList(scanner *bufio.Reader) []string {
+	list := strings.Split(getNextLine(scanner), "")
+	return list
+}
+
+func daisuki() {
+	fp := os.Stdin
+	if len(os.Args) > 1 {
+		fp, _ = os.Open(os.Args[1])
+	}
+	scanner := bufio.NewReader(fp)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var T []int
+	T = getIntList(scanner)
+	N := T[0]
+	strList := []string{}
+	for i := 0; i < N; i++ {
+		S := getNextLine(scanner)
+		strList = append(strList, S)
+	}
+	sort.Slice(strList, func(i, j int) bool { return strList[i] < strList[j] })
+	fmt.Println(strings.Join(strList, ""))
+	writer.Flush()
+}
