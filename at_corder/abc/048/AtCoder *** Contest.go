9a8765d8e89160e358c7333db8f0333096f8e05a
norun111 <u.t012807@gmail.com>
2021-06-21 12:34:01 +0900

    Create AtCoder *** Contest.go

---
+++ at_corder/abc/048/AtCoder *** Contest.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getNextLine(scanner *bufio.Reader) string {
+	var buffer string
+	for {
+		line, isPrefix, _ := scanner.ReadLine()
+		buffer += string(line)
+		if isPrefix == false {
+			break
+		}
+	}
+	return buffer
+}
+
+func getStringList(scanner *bufio.Reader) []string {
+	return strings.Split(getNextLine(scanner), " ")
+}
+
+func getIntList(scanner *bufio.Reader) []int {
+	list := strings.Split(getNextLine(scanner), " ")
+	l := len(list)
+	result := make([]int, l)
+	for i := 0; i < l; i++ {
+		result[i], _ = strconv.Atoi(list[i])
+	}
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fp := os.Stdin
+	if len(os.Args) > 1 {
+		fp, _ = os.Open(os.Args[1])
+	}
+	scanner := bufio.NewReader(fp)
+	writer := bufio.NewWriter(os.Stdout)
+	var S []string
+	S = getStringList(scanner)
+	var uppers []string
+	for _, s := range S {
+		uppercaseLetter := s[0]
+		uppers = append(uppers, string(uppercaseLetter))
+	}
+	fmt.Println(strings.Join(uppers, ""))
+
+	writer.Flush()
+}
